c263be4da32d1139465e4b573d8217d81ceccddc
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-24 11:47:33 -0400

    Resolved font imports, should work on heroku now

--- reverse-hangman-app/src/BackgroundImage.js
+++ reverse-hangman-app/src/BackgroundImage.js
@@ -4,13 +4,12 @@ import Base from './backgroundImage.jpg';
 import {Link} from 'react-router-dom';
 import Home from './HomePage';
 import {Route} from 'react-router-dom/Route';
-import './index.css'
+
 
 class BackgroundImage extends Component {
 
     render() {
         return(
-            
                 <div id="imgContainer">
                     <img src={Base} id="bgImage" />
                     <h1 id="title">Reverse Hangman</h1>
