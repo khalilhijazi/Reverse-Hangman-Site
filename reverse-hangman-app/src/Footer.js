af30e52f31abd1d0850b158849527e60cbc284c1
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-23 23:06:47 -0400

    Adjusted Footer

--- reverse-hangman-app/src/Footer.js
+++ reverse-hangman-app/src/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
             <nav className="navbar navbar-default fixed-bottom bg-dark navbar-dark">
                 <ul className="navbar-nav m-auto">
                     <li className="nav-item">
-                        <span className="navbar-text">Reverse Hangman Inc. &copy; 2018 All rights reserved</span>
+                        <span className="navbar-text">Reverse Hangman &copy; 2018 All rights reserved</span>
                     </li>
                 </ul>
             </nav>
