e5b85f770249de3d8779f9f32d2ed31de585841b
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-26 00:22:04 -0400

    Fixed Build Issues

--- reverse-hangman-app/src/Body.js
+++ reverse-hangman-app/src/Body.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Header from './Header';
-import Footer from './Footer';
-import './Home.css';
 import BackgroundImage from './BackgroundImage';
 import './Body.css';
 
