f4b05d651e8a4dc10bcc202d2a71976c58994d97
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-26 00:59:40 -0400

    Removed Build

--- reverse-hangman-app/src/HomePage.js
+++ reverse-hangman-app/src/HomePage.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Header from './Header';
 import Footer from './Footer';
 import './Home.css';
-import Body from './Body.js';
+import Body from './Body';
 
 class Home extends Component {
     render() {
