995d818a32ba429427c9f54d56d3455f79af0f30
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-26 15:29:23 -0400

    Fixed stick footer content overlap

--- reverse-hangman-app/src/Footer.js
+++ reverse-hangman-app/src/Footer.js
@@ -4,13 +4,9 @@ import './Footer.css';
 class Footer extends Component {
     render() {
         return(
-            <nav className="navbar navbar-default fixed-bottom bg-dark navbar-dark">
-                <ul className="navbar-nav m-auto">
-                    <li className="nav-item">
-                        <span className="navbar-text">Reverse Hangman &copy; 2018 All rights reserved</span>
-                    </li>
-                </ul>
-            </nav>
+            <footer className="footer bg-dark navbar-dark Footer">
+                <span className="navbar-text">Reverse Hangman &copy; 2018 All rights reserved</span>
+            </footer>
         );
     }
 }
