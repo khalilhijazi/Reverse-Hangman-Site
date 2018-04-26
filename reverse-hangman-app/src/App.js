b7e25a91d423fd552f4f3e4e1b4d454a6dd14c2c
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-26 11:43:47 -0400

    Adjusted description wording as well as size, removed all unnecessary imports

--- reverse-hangman-app/src/App.js
+++ reverse-hangman-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
--- reverse-hangman-app/src/BackgroundImage.js
+++ reverse-hangman-app/src/BackgroundImage.js
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
 import './BackgroundImage.css';
 import {Link} from 'react-router-dom';
-import Home from './HomePage';
-import {Route} from 'react-router-dom/Route';
-
 
 class BackgroundImage extends Component {
-
+    //hello
     render() {
         return(
             <div id="imgContainer">
-                <div id="bgContents">
-                    <h1 id="title">Reverse Hangman</h1>
-                    <h2 id="description">Let Your Mind Wander Away</h2>
-                    <Link to='/home'><button className="btn btn-default btn-primary">Get Started</button></Link>
-                </div>
+                <h1 id="title">Reverse Hangman</h1>
+                <h2 id="description">Let Your Mind Wander</h2>
+                <Link to='/home'><button className="btn btn-default btn-primary">Get Started</button></Link>
             </div>
         );
     }
