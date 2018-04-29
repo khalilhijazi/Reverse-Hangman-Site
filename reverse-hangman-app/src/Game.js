e15625e3806a5fecd0b8ea9b69253168bac00597
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-29 14:48:30 -0400

    Added game chatroom using socket.io

--- reverse-hangman-app/src/Game.js
+++ reverse-hangman-app/src/Game.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import './Game.css';
 import Header from './Header';
-
+import Chat from './Chat';
 
 class Game extends Component {
+
     render() {
         return(
             <div className="Game">
                 <Header />
                 <div id="gameBackground">
                     <div id="gameContainer">
-                        
+                        <Chat />
                     </div>
                 </div> 
             </div>
