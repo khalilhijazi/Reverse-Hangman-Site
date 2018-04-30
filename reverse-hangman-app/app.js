b236a0b32fbdb5fd19f6861840007f10c5646eea
Khalil Hijazi <khalilbhijazi@gmail.com>
2018-04-30 05:57:39 -0400

    testing

--- reverse-hangman-app/app.js
+++ reverse-hangman-app/app.js
@@ -1,15 +1,11 @@
 var express = require('express');
-var socket = require('socket.io');
 
 var app = express();
 const PORT = process.env.PORT || 8080;
 
-
-const server = app.listen(PORT, "0.0.0.0", function(){
-    console.log('server is running on port: ' + PORT);
-});
-
-io = socket(server);
+const server = http.createServer(app).listen(config.port); 
+const io = require('socket.io')(server);
+//io = socket(server);
 
 io.on('connection', (socket) => {
     console.log(socket.id);
